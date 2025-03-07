a0e1cf7c054a03f5487ea3eb112d840696a64d93
Vladislav Andreev <andreevvlad443@gmail.com>
2025-03-07 05:44:24 +0300

    chore: minor changes in quick upload api

--- src/main/java/com/enzulode/file/api/QuickUploadController.java
+++ src/main/java/com/enzulode/file/api/QuickUploadController.java
@@ -2,40 +2,42 @@ package com.enzulode.file.api;
 
 import com.enzulode.file.dto.FinishFastUploadDto;
 import com.enzulode.file.dto.PartDetailsDto;
-import com.enzulode.file.service.MultipartUploadService;
+import com.enzulode.file.dto.StartFastUploadResponseDto;
+import com.enzulode.file.facade.UploadApiFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/dev/fast-upload")
 @RequiredArgsConstructor
 public class QuickUploadController {
 
-  private final MultipartUploadService uploadService;
+  private final UploadApiFacade facade;
 
   @GetMapping("/start")
-  public com.enzulode.file.dto.StartFastUploadResponseDto start(
-      @RequestParam("key") String key,
+  public StartFastUploadResponseDto start(
+      @RequestParam("metricsId") Long metricsId,
+      @RequestParam("filename") String filename,
       @RequestParam(name = "type", required = false) String type
   ) {
-    var contentType = "JSON".equalsIgnoreCase(type) ? "application/json" : "text/csv";
-    return uploadService.start(key, contentType);
+    return facade.start(metricsId, filename, type);
   }
 
   @PostMapping("/part")
   public PartDetailsDto uploadPart(
       @RequestParam("uploadId") String uploadId,
       @RequestParam("partNumber") int partNumber,
-      @RequestParam("key") String key,
+      @RequestParam("fileId") UUID fileId,
       @RequestPart("part") MultipartFile part
   ) {
-    return uploadService.uploadPart(uploadId, partNumber, key, part);
+    return facade.uploadPart(uploadId, partNumber, fileId, part);
   }
 
   @PostMapping("/finish")
   public void finish(@RequestBody FinishFastUploadDto finishFastUploadDto) {
-    uploadService.finish(finishFastUploadDto);
+    facade.finish(finishFastUploadDto);
   }
 }
